1b6e24614755f1526503753b30a5e7531bb26e82
刘兴 <liuxing997@foxmail.com>
2023-02-17 20:04:21 +0800

    增加TypeScript图标

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,6 +2,7 @@
   <h1>{{ msg }}</h1>
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
+    &emsp;
     <button type="button" @click="store.count++">
       pinia count is {{ store.count }}
     </button>
